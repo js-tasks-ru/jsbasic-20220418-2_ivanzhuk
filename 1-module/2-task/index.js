abe4d3dc27cfad73d48f55a95f4eb9ca28e4d737
Zhuk Ivan <zhukekIvan@yandex.ru>
2022-04-19 18:23:02 +0300

    1-module_ready

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,14 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (typeof name !== "string") {
+    return false
+  }
+  if (name.length >= 4 && !name.includes(' ')) {
+    return true
+  } else {
+    return false
+  }
 }
 
 function sayHello() {
