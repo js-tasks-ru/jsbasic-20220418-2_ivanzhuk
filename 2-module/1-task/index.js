01e9905c7c956567d43a6bb2264f1b77b994df56
Zhuk Ivan <zhukekIvan@yandex.ru>
2022-04-22 16:45:43 +0300

    module_2-ready

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0;
+  const values = Object.values(salaries);
+  values.forEach((value) => {
+    if (isFinite(value) && !isNaN(value)) {
+      result += value;
+    }
+  })
+  return result
 }
